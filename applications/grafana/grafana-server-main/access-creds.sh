8119a9fcc344f5871b34d2140561f05b5e2749b3
Pat Riehecky <riehecky@fnal.gov>
2023-08-14 12:54:50 -0500

    Fix missing dot

--- applications/grafana/grafana-server-main/access-creds.sh
+++ applications/grafana/grafana-server-main/access-creds.sh
@@ -1,8 +1,8 @@
 NAMESPACE="monitoring"
 HELM_RELEASE="grafana"
 
-ADMIN_USER=$(kubectl -n ${NAMESPACE} get secret ${HELM_RELEASE} -ojsonpath='{data.admin-user}' | base64 --decode; echo)
-ADMIN_PASS=$(kubectl -n ${NAMESPACE} get secret ${HELM_RELEASE} -ojsonpath='{data.admin-password}' | base64 --decode; echo)
+ADMIN_USER=$(kubectl -n ${NAMESPACE} get secret ${HELM_RELEASE} -ojsonpath='{.data.admin-user}' | base64 --decode; echo)
+ADMIN_PASS=$(kubectl -n ${NAMESPACE} get secret ${HELM_RELEASE} -ojsonpath='{.data.admin-password}' | base64 --decode; echo)
 
 printf "%0.s-" {1..30}
 echo ""
