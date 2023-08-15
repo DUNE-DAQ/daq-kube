4e8db62307a945575870eddcab2001ac7a469c63
Pat Riehecky <riehecky@fnal.gov>
2023-08-15 15:25:08 -0500

    Add credentials

---
+++ dune_daq/opmon/influxdb/access-creds.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAMESPACE="opmon"
+HELM_RELEASE="opmon-influxdb"
+
+DATABASE="influxv3"
+HOST=${HELM_RELEASE}
+PASS=$(kubectl -n ${NAMESPACE} get secret ${HELM_RELEASE}-auth -ojsonpath="{.data.influxdb-password}" | base64 --decode; echo)
+PORT="8086"
+USER=$(kubectl -n ${NAMESPACE} get secret ${HELM_RELEASE}-auth -ojsonpath="{.data.influxdb-user}" | base64 --decode; echo)
+
+echo "--------------------------------------------------"
+echo "DUNE DAQ OpMon Service InfluxDB:"
+echo "  URI in cluster: influxdb://${USER}:${PASS}@${HOST}.${NAMESPACE}.svc:${PORT}/${DATABASE}"
+echo "--------------------------------------------------"
