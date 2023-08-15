2cbab2d2d64fd78bb1471afc6459be1cd4f9d894
Pat Riehecky <riehecky@fnal.gov>
2023-08-15 14:36:34 -0500

    quoteval

--- baseline/prometheus/alertmanagers/access-creds.sh
+++ baseline/prometheus/alertmanagers/access-creds.sh
@@ -6,7 +6,7 @@ printf "%0.s-" {1..50}
 echo ""
 echo "Prometheus Alert Managers:"
 for alerts in $(kubectl -n ${NAMESPACE} --no-headers=true get alertmanager -o custom-columns=":metadata.name"); do
-  svc_name=$(kubectl -n ${NAMESPACE} --no-headers=true get service -l app=alertmanager-${alert} -o custom-columns=":metadata.name")
+  svc_name=$(kubectl -n ${NAMESPACE} --no-headers=true get service -l "app=alertmanager-${alert}" -o custom-columns=":metadata.name")
   echo "  URL in cluster for ${alerts}: http://${svc_name}.${NAMESPACE}.svc:9093"
 done
 printf "%0.s-" {1..50}
--- baseline/prometheus/prometheis/access-creds.sh
+++ baseline/prometheus/prometheis/access-creds.sh
@@ -6,7 +6,7 @@ printf "%0.s-" {1..50}
 echo ""
 echo "Prometheus Servers:"
 for prom in $(kubectl -n ${NAMESPACE} --no-headers=true get prometheus -o custom-columns=":metadata.name"); do
-  svc_name=$(kubectl -n ${NAMESPACE} --no-headers=true get service -l app=prometheus-${alert} -o custom-columns=":metadata.name")
+  svc_name=$(kubectl -n ${NAMESPACE} --no-headers=true get service -l "app=prometheus-${prom}" -o custom-columns=":metadata.name")
   echo "  URL in cluster for ${prom}: http://${svc_name}.${NAMESPACE}.svc:9090"
 done
 printf "%0.s-" {1..50}
