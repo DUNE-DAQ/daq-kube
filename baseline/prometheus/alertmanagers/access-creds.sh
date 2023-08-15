98217d73a83a8b1fc03bb2b84de2a535a1dae124
Pat Riehecky <riehecky@fnal.gov>
2023-08-15 14:34:44 -0500

    Try to fixup selections

--- baseline/prometheus/alertmanagers/access-creds.sh
+++ baseline/prometheus/alertmanagers/access-creds.sh
@@ -6,7 +6,8 @@ printf "%0.s-" {1..50}
 echo ""
 echo "Prometheus Alert Managers:"
 for alerts in $(kubectl -n ${NAMESPACE} --no-headers=true get alertmanager -o custom-columns=":metadata.name"); do
-  echo "  URL in cluster: http://${alerts}.${NAMESPACE}.svc:9093"
+  svc_name=$(kubectl -n ${NAMESPACE} --no-headers=true get service -l app=alertmanager-${alert} -o custom-columns=":metadata.name")
+  echo "  URL in cluster for ${alerts}: http://${svc_name}.${NAMESPACE}.svc:9093"
 done
 printf "%0.s-" {1..50}
 echo ""
--- baseline/prometheus/prometheis/access-creds.sh
+++ baseline/prometheus/prometheis/access-creds.sh
@@ -6,7 +6,8 @@ printf "%0.s-" {1..50}
 echo ""
 echo "Prometheus Servers:"
 for prom in $(kubectl -n ${NAMESPACE} --no-headers=true get prometheus -o custom-columns=":metadata.name"); do
-  echo "  URL in cluster: http://${prom}.${NAMESPACE}.svc:9090"
+  svc_name=$(kubectl -n ${NAMESPACE} --no-headers=true get service -l app=prometheus-${alert} -o custom-columns=":metadata.name")
+  echo "  URL in cluster for ${prom}: http://${svc_name}.${NAMESPACE}.svc:9090"
 done
 printf "%0.s-" {1..50}
 echo ""
