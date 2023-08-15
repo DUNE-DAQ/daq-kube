66cef4020fe03fde8c184b33ea7c3d29b6e319f0
Pat Riehecky <riehecky@fnal.gov>
2023-08-15 15:18:25 -0500

    Fix typo

--- dune_daq/node-ports/access-creds.sh
+++ dune_daq/node-ports/access-creds.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 echo "--------------------------------------------------"
 echo "DUNE DAQ Node Ports for Cluster:"
 (
  cd $(dirname $0)
  for port in $(grep nodePort *.yaml | tr -d ' ' | tr -d '#'); do
-  NAME=$(echo $port | sed -e 's/\.yaml:nodePort:/ is /'
+  NAME=$(echo $port | sed -e 's/\.yaml:nodePort:/ is /')
   echo "  ${NAME}"
  done
 )
