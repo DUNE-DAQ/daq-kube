7335115038a6588260f05d74e5286d76a139e4ce
Pat Riehecky <riehecky@fnal.gov>
2023-10-30 15:00:13 -0500

    Update notes

--- dune_daq_services/kafka/kafka-server/access-creds.sh
+++ dune_daq_services/kafka/kafka-server/access-creds.sh
@@ -3,6 +3,7 @@
 NAMESPACE="kafka"
 RELEASE="dune-daq"
 
+kubectl -n ${NAMESPACE} get pods >/dev/null
 echo "--------------------------------------------------"
 echo "DUNE DAQ Kafka Service:"
 echo "  URL in cluster: kafka://${RELEASE}-kafka-bootstrap.${NAMESPACE}.svc:9092"
--- print-creds.sh
+++ print-creds.sh
@@ -2,6 +2,7 @@
 
 echo ''
 echo '  Printing Access Information:'
+echo '  Some services may error out and provide incomplete information'
 echo ''
 for CRED in $(find * -type f -name access-creds.sh -executable | sort); do
   ${CRED}
