4a51e155642c528f7f10d8eaf3e82b1121a1bcaf
Pat Riehecky <riehecky@fnal.gov>
2025-07-21 09:04:48 -0500

    Drop daqconfig per chat

--- dune_daq_services/daqconfig/daqconfig/access-creds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAMESPACE="daqconfig"
-
-kubectl -n ${NAMESPACE} >/dev/null
-echo "--------------------------------------------------"
-echo "DUNE DAQ Config Service:"
-echo "  URL in cluster: https://daqconfig.${NAMESPACE}.svc:5003"
-echo "--------------------------------------------------"
--- dune_daq_services/daqconfig/mongo-express/access-creds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAMESPACE="daqconfig"
-
-kubectl -n ${NAMESPACE} >/dev/null
-echo "--------------------------------------------------"
-echo "DUNE DAQ Config Service Mongo Express:"
-echo "  URL in cluster: http://mongodb-express.${NAMESPACE}.svc:8081"
-echo "--------------------------------------------------"
