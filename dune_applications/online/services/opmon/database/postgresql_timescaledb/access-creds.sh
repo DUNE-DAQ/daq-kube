1891861c2a483c2fcb270f3ddb241d5f4dba1088
Pat Riehecky <riehecky@fnal.gov>
2026-05-28 15:08:02 -0500

    Start work on timescaledb extension

---
+++ dune_applications/online/services/opmon/database/postgresql_timescaledb/access-creds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NAMESPACE="opmon"
+CLUSTER="opmon-timescaledb"
+
+echo "--------------------------------------------------"
+kubectl get namespaces | grep -q ${NAMESPACE}
+if [[ $? -eq 0 ]]; then
+    URI_RW=$(
+        kubectl -n ${NAMESPACE} get secret ${CLUSTER}-opmon-password -ojsonpath="{.data.uri}" | base64 --decode;
+        echo
+    )
+    URI_RO=$(
+        kubectl -n ${NAMESPACE} get secret ${CLUSTER}-readonly-password -ojsonpath="{.data.uri}" | base64 --decode;
+        echo
+    )
+
+    echo "DUNE DAQ Opmon Service Database URI:"
+    echo "  Read-Write URI in cluster: ${URI_RW}"
+    echo "  Read-Only URI in cluster : ${URI_RO}"
+else
+    echo "Kubernetes namespace ${NAMESPACE} does not exist"
+fi
+echo "--------------------------------------------------"
